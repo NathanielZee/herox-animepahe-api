0367e1520cad7968604c3c30abb5b69e805e6d42
ElijahCodes12345 <oluwafemiadebanwo@gmail.com>
2025-05-28 15:59:26 +0100

    removed unused code

--- models/homeModel.js
+++ models/homeModel.js
@@ -8,11 +8,7 @@ class HomeModel {
         const results = await Animepahe.getData("airing", { page });
 
         if (!results || !results.data) {
-            const htmlData = await this.scrapeHomePage();
-            if (!htmlData || htmlData.length === 0) {
-                throw new CustomError('No airing anime data found', 404);
-            }
-            return htmlData;
+            throw new CustomError('No airing anime data found', 404);
         }
 
         return DataProcessor.processApiData(results);
@@ -30,27 +26,6 @@ class HomeModel {
         }
 
         return DataProcessor.processApiData(results, 'search');
-    }
-    
-    static async scrapeHomePage() {
-        const url = Config.getUrl('home');
-        const $ = await this.fetchPage(url);
-        
-        if (!$) {
-            throw new CustomError('Failed to load home page', 503);
-        }
-
-        const airingAnime = [];
-        $('.episode-list .episode-wrap').each((i, element) => {
-            airingAnime.push({
-                title: $(element).find('.episode .episode-title-warap a').text().trim(),
-                episode: $(element).find('.episode .episode-number').text().trim(),
-                image: $(element).find('.episode .episode-snapshot img').attr('src'),
-                link: `${Config.baseUrl}${$(element).find('a').attr('href')}`
-            });
-        });
-         
-        return airingAnime;
     }
 }
 
